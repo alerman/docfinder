468b36c487be8f96a6580499bb8ed3860e6c4a82
Adam Lerman <alerman@gmail.com>
2019-05-09 19:10:41 -0400

    Working

--- docfinder.web/src/main/resources/static/javascript/root.js
+++ docfinder.web/src/main/resources/static/javascript/root.js
@@ -1,3 +1,5 @@
+
+
 angular.module("root", ["filters", "ngResource"])
     .controller("index", ["$scope", "$resource", "$location",
         function ($scope, $resource, $location) {
@@ -14,6 +16,7 @@ angular.module("root")
         function ($scope, $resource,$location) {
             console.log($location.search().docId);
 
+            console.log($location.search().searchTerm);
             var srchUrl = $resource($location.protocol() + "://" +
                 $location.host() + ":" + 8983 +
                 "/solr/gettingstarted/select?q=id%3A" + $location.search().docId + "&wt=json");
